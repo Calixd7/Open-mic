3f5661e0656845525a271535c73cf26028b9a00a
Calixd7 <calixd7@gmail.com>
2021-02-18 12:30:41 -0500

    djoser, djangorest, and heroku installation

--- Backend/openmic/settings.py
+++ Backend/openmic/settings.py
@@ -144,6 +144,15 @@ INTERNAL_IPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',),
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+       'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
 }
 CORS_ALLOW_ALL_ORIGINS = True
 
