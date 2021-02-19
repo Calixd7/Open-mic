12909be3d8aba58ec7b13c22e4c4027f03d3d59c
Matthew Smith <matthewbenton224@gmail.com>
2021-02-19 10:34:50 -0500

    removed change button for photo and removed 'tell us about yourself' under about

--- frontend/src/components/IndividualProfileSetup.js
+++ frontend/src/components/IndividualProfileSetup.js
@@ -17,9 +17,9 @@ function IndividualProfileSetup () {
           About
         </label>
         <div className='mt-1'>
-          <textarea id='about' name='about' rows='3' className='shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block-w-full sm:text-sm border-gray-300 rounded-md' />
+          <textarea id='about' name='about' rows='3' className='shadow-sm focus:ring-indigo-300 focus:border-indigo-500 block-w-full sm:text-sm border-gray-300 rounded-md' />
         </div>
-        <p className='mt-2 text-sm text-gray-500'>Tell Us About Yourself</p>
+
       </div>
 
       <div className='sm:col-span-6'>
@@ -32,9 +32,6 @@ function IndividualProfileSetup () {
               <path d='M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 44 0 018 0z' />
             </svg>
           </span>
-          <button type='button' className='ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 cofus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'>
-            Change
-          </button>
           <div class='mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md'>
             <div class='space-y-1 text-center'>
               <svg class='mx-auto h-12 w-12 text-gray-400' stroke='currentColor' fill='none' viewBox='0 0 48 48' aria-hidden='true'>
