c7e3a8815dac85044af0cdfaf4b55a5b9c6d3b92
Daniel McKinney <adrmckinney@gmail.com>
2021-02-22 01:44:19 -0500

    formatted the login and registration pages so that the forms are in the center of page. Also styled the profile pages a bit.

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -30,77 +30,78 @@ function Registration ({ setAuth, isLoggedIn }) {
   }
 
   return (
-    <div>
-      <form
-        className='mt-8 space-y-6'
-        onSubmit={handleRegistration}
-      >
-        {errors && (
-          <div>{errors}</div>
-        )}
-        <input type='hidden' name='remember' value='true' />
-        <div className='rounded-md shadow-sm -space-y-px'>
-          <div>
-            <label
-              htmlFor='username'
-              className='sr-only'
-            >
-              Username
-            </label>
-            <input
-              id='username'
-              name='username'
-              type='username'
-              autoComplete='username'
-              required className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-              placeholder='Username'
-              value={username}
-              onChange={event => setUsername(event.target.value)}
-            />
-          </div>
-          <div>
-            <label
-              htmlFor='password'
-              className='sr-only'
-            >
-              Password
-            </label>
-            <input
-              id='password'
-              name='password'
-              type='password'
-              autoComplete='current-password'
-              required className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-              placeholder='Password'
-              value={password}
-              onChange={event => setPassword(event.target.value)}
-            />
-          </div>
-          <div className='flex items-center justify-between'>
-            <div className='flex items-center'>
-              <input
-                id='remember_me'
-                name='remember_me'
-                type='checkbox'
-                className='h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
-              />
+    <div className='min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8'>
+      <div className='max-w-md w-full space-y-8'>
+        <form
+          className='mt-8 space-y-6'
+          onSubmit={handleRegistration}
+        >
+          {errors && (
+            <div>{errors}</div>
+          )}
+          <input type='hidden' name='remember' value='true' />
+          <div className='rounded-md shadow-sm -space-y-px'>
+            <div>
               <label
-                htmlFor='remember_me'
-                className='ml-2 block text-sm text-gray-900'
+                htmlFor='username'
+                className='sr-only'
               >
-                Remember Me
+                Username
               </label>
+              <input
+                id='username'
+                name='username'
+                type='username'
+                autoComplete='username'
+                required className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                placeholder='Username'
+                value={username}
+                onChange={event => setUsername(event.target.value)}
+              />
             </div>
-          </div>
-          <div>
-            <button
-              type='submit'
-              className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-            >
-              <span
-                className='absolute left-0 inset-y-0 flex items-center pl-3'
+            <div>
+              <label
+                htmlFor='password'
+                className='sr-only'
               >
-                {/* <svg
+                Password
+              </label>
+              <input
+                id='password'
+                name='password'
+                type='password'
+                autoComplete='current-password'
+                required className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                placeholder='Password'
+                value={password}
+                onChange={event => setPassword(event.target.value)}
+              />
+            </div>
+            <div className='flex items-center justify-between'>
+              <div className='flex items-center my-4'>
+                <input
+                  id='remember_me'
+                  name='remember_me'
+                  type='checkbox'
+                  className='h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
+                />
+                <label
+                  htmlFor='remember_me'
+                  className='ml-2 block text-sm text-gray-900'
+                >
+                  Remember Me
+                </label>
+              </div>
+            </div>
+            <div>
+              <button
+                type='submit'
+                className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+              >
+                <span
+                  className='absolute left-0 inset-y-0 flex items-center pl-3'
+                >
+                  {/* <svg
                     className='h-5 w-5 text-indigo-500 group-hover:text-indigo-400'
                     xmlns='http://www.w3.org/2000/svg'
                     viewBox='0 0 20'
@@ -113,12 +114,13 @@ function Registration ({ setAuth, isLoggedIn }) {
                       clipRule='evenodd'
                     />
                   </svg> */}
-              </span>
-              Complete Your Profile
-            </button>
+                </span>
+                Complete Your Profile
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
