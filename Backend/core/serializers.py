f468c0acbf7bca5112ffda71cb73ab151ff2b2ba
Calixd7 <calixd7@gmail.com>
2021-02-22 23:24:29 -0500

    list change

--- Backend/core/serializers.py
+++ Backend/core/serializers.py
@@ -24,23 +24,23 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
-class IndGenreSerializer (serializers.ModelSerializer):
-    class Meta:
-        model = UserProfile
-        fields = ["id", 'genre']
+# class IndGenreSerializer (serializers.ModelSerializer):
+#     class Meta:
+#         model = UserProfile
+#         fields = ["id", 'genre']
 
-class IndInstrumentSerializer (serializers.ModelSerializer):
-    class Meta:
-        model = UserProfile
-        fields = ["id", 'instrument']
+# class IndInstrumentSerializer (serializers.ModelSerializer):
+#     class Meta:
+#         model = UserProfile
+#         fields = ["id", 'instrument']
 
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
     ind = serializers.SlugRelatedField(read_only=True, slug_field='username')
     followers = serializers.StringRelatedField(many=True, read_only=True)
-    instrument = IndInstrumentSerializer(many=True)
-    genre = IndGenreSerializer(many=True)
+    # instrument = IndInstrumentSerializer(many=True)
+    # genre = IndGenreSerializer(many=True)
     class Meta:
         model = UserProfile
         fields = [
@@ -54,22 +54,22 @@ class UserProfileSerializer(serializers.ModelSerializer):
             'followers'
 
         ]
-class GenreSerializer (serializers.ModelSerializer):
-    class Meta:
-        model = BandProfile
-        fields = ["id", 'band_genre']
+# class GenreSerializer (serializers.ModelSerializer):
+#     class Meta:
+#         model = BandProfile
+#         fields = ["id", 'band_genre']
 
-class InstrumentSerializer (serializers.ModelSerializer):
-    class Meta:
-        model = BandProfile
-        fields = ["id", 'band_instruments']
+# class InstrumentSerializer (serializers.ModelSerializer):
+#     class Meta:
+#         model = BandProfile
+#         fields = ["id", 'band_instruments']
 
 class BandProfileSerializer(serializers.ModelSerializer):
     follows = serializers.StringRelatedField(many=True, read_only=True)
     followers = serializers.StringRelatedField(many=True, read_only=True)
     band = serializers.SlugRelatedField(read_only=True, slug_field='username')
-    band_genre = GenreSerializer(many=True)
-    band_instruments =InstrumentSerializer(many=True)
+    # band_genre = GenreSerializer(many=True)
+    # band_instruments =InstrumentSerializer(many=True)
     class Meta:
         model = BandProfile  
         fields = [
