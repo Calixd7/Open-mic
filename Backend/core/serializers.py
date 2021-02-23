ff4094b77b149b6286fbcae32027a6bc778b0b58
Calixd7 <calixd7@gmail.com>
2021-02-22 19:28:40 -0500

    user profile and bandprofile fix

--- Backend/core/serializers.py
+++ Backend/core/serializers.py
@@ -24,9 +24,23 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
 
+class IndGenreSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ["id", 'genre']
+
+class IndInstrumentSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ["id", 'instrument']
+
+
+
 class UserProfileSerializer(serializers.ModelSerializer):
     ind = serializers.SlugRelatedField(read_only=True, slug_field='username')
     followers = serializers.StringRelatedField(many=True, read_only=True)
+    instrument = IndInstrumentSerializer(many=True)
+    genre = IndGenreSerializer(many=True)
     class Meta:
         model = UserProfile
         fields = [
@@ -72,19 +86,7 @@ class BandProfileSerializer(serializers.ModelSerializer):
             "followers",
             "follows",
         ]
-    def create(self, validated_data):
-        genre_data = validated_data.pop('band_genre')
-        BandProfile = BandProfile.objects.create(**validated_data)
-        for genre_data in genre_data:
-            band_genre.objects.create(BandProfile=BandProfile, **genre_data)
-        return BandProfile
-
-    def create(self, validated_data):
-        instrument_data = validated_data.pop('band_instruments')
-        BandProfile = BandProfile.objects.create(**validated_data)
-        for instrument_data in instrument_data:
-            band_instruments.objects.create(BandProfile=BandProfile, **instrument_data)
-        return BandProfile
+    
 
 class FollowSerializer(serializers.ModelSerializer):
     follows = serializers.StringRelatedField(many=True, read_only=True)
