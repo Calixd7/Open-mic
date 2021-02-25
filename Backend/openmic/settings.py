171dc77aa513ec81866532caa5662392060e0e78
Calixd7 <calixd7@gmail.com>
2021-02-25 11:44:00 -0500

    settings cors

--- Backend/openmic/settings.py
+++ Backend/openmic/settings.py
@@ -70,7 +70,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-  
 ]
 
 ROOT_URLCONF = 'openmic.urls'
