a60f01a2892ebb03e1b23f41087852c6f13945ca
Calixd7 <calixd7@gmail.com>
2021-02-24 22:31:36 -0500

    null value on wanted instruments

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -72,7 +72,7 @@ class UserProfile(models.Model):
     years_active = models.CharField(max_length=100, blank=True, null=True)
     vacancy = models.BooleanField(default=False)
     individualorband = models.CharField(max_length=100, choices=OPTIONS, null=True)
-    wanted_instruments = models.ManyToManyField(to=WantedInstruments, related_name='users', blank=True)
+    wanted_instruments = models.ManyToManyField(to=WantedInstruments, related_name='users', blank=True, null=True)
     wanted_info = models.CharField(max_length=500, blank=True, null=True )
                                                                             
     
