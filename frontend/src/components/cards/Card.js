d85b216351cc8409e87d7e15931b1bac5ac1da78
Daniel McKinney <adrmckinney@gmail.com>
2021-02-26 13:08:10 -0500

    added margins and adjusted padding for card btns.

--- frontend/src/components/cards/Card.js
+++ frontend/src/components/cards/Card.js
@@ -37,13 +37,13 @@ function Card ({ cards, profile }) {
               <div className='mt=px flex divide-x divide-gray-200'>
                 <div className='w-0 flex-1 flex'>
                   <div
-                    className='relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500'
+                    className='relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-2 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500'
                   >
-                    <span className='w-full relative z-0 flex flex-col shadow-sm rounded-md'>
+                    <span className='mx-2 w-full relative z-0 flex flex-col shadow-sm rounded-md'>
 
                       <button
                         type='button'
-                        className='justify-center w-full inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                        className='justify-center w-full inline-flex items-center px-2 py-1 mb-1 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                         onClick={() => history.push(`view-card/${card.pk}`)}
                       >
                         <span>More Info</span>
@@ -52,18 +52,16 @@ function Card ({ cards, profile }) {
                       <div className='relative z-0 inline-flex shadow-sm rounded-md'>
                         <button
                           type='button'
-                          className='justify-center inline-flex flex-1 items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                          className='justify-center inline-flex flex-1 items-center px-2 py-1 mr-1 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                           // onClick={() => history.push(`view-card/${card.pk}`)}
                         >
-
                           <span><Follow card={card} /></span>
                         </button>
                         <button
                           type='button'
-                          className='justify-center inline-flex flex-1 items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                          className='justify-center inline-flex flex-1 items-center px-2 py-1 ml-1 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                           onClick={() => history.push(`message/${card.pk}`)}
                         >
-
                           <span><MessageBtn card={card} /></span>
                         </button>
                       </div>
