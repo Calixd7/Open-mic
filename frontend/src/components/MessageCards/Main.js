067446a0bc32e8baa1e847a41fd4b17e65c10d16
Matthew Smith <matthewbenton224@gmail.com>
2021-02-28 11:21:13 -0500

    styling fixes on message page

--- frontend/src/components/MessageCards/Main.js
+++ frontend/src/components/MessageCards/Main.js
@@ -270,40 +270,41 @@ function Main () {
           </div>
           {/* <!-- Message header --> */}
         </div>
-
-        <div className='min-h-0 flex-1 overflow-y-auto'>
-          <div className='bg-white pt-5 pb-6 shadow'>
-            <div className='px-4 sm:flex sm:justify-between sm:items-baseline sm:px-6 lg:px-8'>
-              <div className='sm:w-0 sm:flex-1'>
-                <h1
-                  id='message-heading'
-                  className='text-lg font-medium text-gray-900'
-                >
-                  Re: New pricing for existing customers
-                </h1>
-                <p className='mt-1 text-sm text-gray-500 truncate'>
-                  joearmstrong@example.com
-                </p>
+        <div>
+          <div className='min-h-0 flex-1 overflow-y-auto'>
+            <div className='bg-white pt-5 pb-6 shadow'>
+              <div className='px-4 sm:flex sm:justify-between sm:items-baseline sm:px-6 lg:px-8'>
+                <div className='sm:w-0 sm:flex-1'>
+                  <h1
+                    id='message-heading'
+                    className='text-lg font-medium text-gray-900'
+                  >
+                    Re: New pricing for existing customers
+                  </h1>
+                  <p className='mt-1 text-sm text-gray-500 truncate'>
+                    joearmstrong@example.com
+                  </p>
+                </div>
               </div>
 
               <div className='mt-4 flex items-center justify-between sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:justify-start'>
-                <span className='inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-cyan-100 text-cyan-800'>
+                {/* <span className='inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-cyan-100 text-cyan-800'>
                   Open
                 </span>
                 <div className='ml-3 relative inline-block text-left'>
-                  <div>
-                    <button
-                      type='button'
-                      className='-my-2 p-2 rounded-full bg-white flex items-center text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600'
-                      id='menu-4'
-                      aria-expanded='false'
-                      aria-haspopup='true'
-                    >
-                      <span className='sr-only'>
-                        Open options
-                      </span>
-                      {/* <!-- Heroicon name: solid/dots-vertical --> */}
-                      <svg
+                  <div> */}
+                {/* <button
+                type='button'
+                className='-my-2 p-2 rounded-full bg-white flex items-center text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-600'
+                id='menu-4'
+                aria-expanded='false'
+                aria-haspopup='true'
+              >
+                <span className='sr-only'>
+                  Open options
+                </span> */}
+                {/* <!-- Heroicon name: solid/dots-vertical --> */}
+                {/* <svg
                         className='h-5 w-5'
                         xmlns='http://www.w3.org/2000/svg'
                         viewBox='0 0 20 20'
@@ -313,9 +314,9 @@ function Main () {
                         <path d='M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z' />
                       </svg>
                     </button>
-                  </div>
+                  </div> */}
 
-                  {/* <!--
+                {/* <!--
                     Dropdown menu, show/hide based on menu state.
 
                     Entering: "transition ease-out duration-100"
@@ -325,130 +326,133 @@ function Main () {
                       From: "transform opacity-100 scale-100"
                       To: "transform opacity-0 scale-95"
                   --> */}
-                  <div
-                    className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'
-                    role='menu'
-                    aria-orientation='vertical'
-                    aria-labelledby='menu-4'
+                {/* <div
+                className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'
+                role='menu'
+                aria-orientation='vertical'
+                aria-labelledby='menu-4'
+              >
+                <div className='py-1'>
+                  <button
+                    type='button'
+                    className='w-full flex justify-between px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900'
+                    role='menuitem'
                   >
-                    <div className='py-1'>
-                      <button
-                        type='button'
-                        className='w-full flex justify-between px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900'
-                        role='menuitem'
-                      >
-                        <span>
-                          Copy email address
-                        </span>
-                      </button>
-                      <Link
-                        to='#'
-                        className='flex justify-between px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900'
-                        role='menuitem'
-                      >
-                        <span>
-                          Previous conversations
-                        </span>
-                      </Link>
-                      <Link
-                        to='#'
-                        className='flex justify-between px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900'
-                        role='menuitem'
-                      >
-                        <span>
-                          View original
-                        </span>
-                      </Link>
-                    </div>
-                  </div>
+                    <span>
+                      Copy email address
+                    </span>
+                  </button>
+                  <Link
+                    to='#'
+                    className='flex justify-between px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900'
+                    role='menuitem'
+                  >
+                    <span>
+                      Previous conversations
+                    </span>
+                  </Link>
+                  <Link
+                    to='#'
+                    className='flex justify-between px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900'
+                    role='menuitem'
+                  >
+                    <span>
+                      View original
+                    </span>
+                  </Link>
                 </div>
+              </div>
+            </div> */}
+                {/* </div>
+            </div> */}
+                {/* </div> */}
+                {/* <!-- Thread section--> */}
+                <ul className='py-4 space-y-2 sm:px-6 sm:space-y-4 lg:px-8'>
+                  <li className='bg-white px-4 py-6 shadow sm:rounded-lg sm:px-6'>
+                    <div className='sm:flex sm:justify-between sm:items-baseline'>
+                      <h3 className='text-base font-medium'>
+                        <span className='text-gray-900'>
+                          Joe Armstrong
+                          <span className='text-gray-600'>
+                            wrote
+                          </span>
+                        </span>
+                      </h3>
+                      <p className='mt-1 text-sm text-gray-600 whitespace-nowrap sm:mt-0 sm:ml-3'>
+                        <time dateTime='2021-01-28T19:24'>
+                          Yesterday at 7:24am
+                        </time>
+                      </p>
+                    </div>
+                    <div className='mt-4 space-y-6 text-sm text-gray-800'>
+                      <p>Thanks so much! Can't wait to try it out.</p>
+                    </div>
+                  </li>
+
+                  <li className='bg-white px-4 py-6 shadow sm:rounded-lg sm:px-6'>
+                    <div className='sm:flex sm:justify-between sm:items-baseline'>
+                      <h3 className='text-base font-medium'>
+                        <span className='text-gray-900'>
+                          Monica White
+                        </span>
+                        <span className='text-gray-600'>
+                          wrote
+                        </span>
+                      </h3>
+                      <p className='mt-1 text-sm text-gray-600 whitespace-nowrap sm:mt-0 sm:ml-3'>
+                        <time dateTime='2021-01-27T16:35'>
+                          Wednesday at 4:35pm
+                        </time>
+                      </p>
+                    </div>
+                    <div className='mt-4 space-y-6 text-sm text-gray-800'>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Malesuada at ultricies tincidunt elit et, enim. Habitant nunc, adipiscing non fermentum, sed est a, aliquet. Lorem in vel libero vel augue aliquet dui commodo.
+                      </p>
+                      <p>
+                        Nec malesuada sed sit ut aliquet. Cras ac pharetra, sapien purus vitae vestibulum auctor faucibus ullamcorper. Leo quam tincidunt porttitor neque, velit sed. Tortor mauris ornare ut tellus sed aliquet amet venenatis condimentum. Convallis accumsan et nunc eleifend.
+                      </p>
+                      <p>
+                        <strong className='font-semibold'>
+                          Monica White
+                        </strong>
+                        Customer Service
+                      </p>
+                    </div>
+                  </li>
+
+                  <li className='bg-white px-4 py-6 shadow sm:rounded-lg sm:px-6'>
+                    <div className='sm:flex sm:justify-between sm:items-baseline'>
+                      <h3 className='text-base font-medium'>
+                        <span className='text-gray-900'>
+                          Joe Armstrong
+                        </span>
+                        <span className='text-gray-600'>
+                          wrote
+                        </span>
+                      </h3>
+                      <p className='mt-1 text-sm text-gray-600 whitespace-nowrap sm:mt-0 sm:ml-3'>
+                        <time dateTime='2021-01-27T16:09'>
+                          Wednesday at 4:09pm
+                        </time>
+                      </p>
+                    </div>
+                    <div className='mt-4 space-y-6 text-sm text-gray-800'>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Malesuada at ultricies tincidunt elit et, enim. Habitant nunc, adipiscing non fermentum, sed est a, aliquet. Lorem in vel libero vel augue aliquet dui commodo.
+                      </p>
+                      <p>
+                        Nec malesuada sed sit ut aliquet. Cras ac pharetra, sapien purus vitae vestibulum auctor faucibus ullamcorper. Leo quam tincidunt porttitor neque, velit sed. Tortor mauris ornare ut tellus sed aliquet amet venenatis condimentum. Convallis accumsan et nunc eleifend.
+                      </p>
+                      <p>
+                        – Joe
+                      </p>
+                    </div>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
-          {/* <!-- Thread section--> */}
-          <ul className='py-4 space-y-2 sm:px-6 sm:space-y-4 lg:px-8'>
-            <li className='bg-white px-4 py-6 shadow sm:rounded-lg sm:px-6'>
-              <div className='sm:flex sm:justify-between sm:items-baseline'>
-                <h3 className='text-base font-medium'>
-                  <span className='text-gray-900'>
-                    Joe Armstrong
-                  </span>
-                  <span className='text-gray-600'>
-                    wrote
-                  </span>
-                </h3>
-                <p className='mt-1 text-sm text-gray-600 whitespace-nowrap sm:mt-0 sm:ml-3'>
-                  <time dateTime='2021-01-28T19:24'>
-                    Yesterday at 7:24am
-                  </time>
-                </p>
-              </div>
-              <div className='mt-4 space-y-6 text-sm text-gray-800'>
-                <p>Thanks so much! Can't wait to try it out.</p>
-              </div>
-            </li>
-
-            <li className='bg-white px-4 py-6 shadow sm:rounded-lg sm:px-6'>
-              <div className='sm:flex sm:justify-between sm:items-baseline'>
-                <h3 className='text-base font-medium'>
-                  <span className='text-gray-900'>
-                    Monica White
-                  </span>
-                  <span className='text-gray-600'>
-                    wrote
-                  </span>
-                </h3>
-                <p className='mt-1 text-sm text-gray-600 whitespace-nowrap sm:mt-0 sm:ml-3'>
-                  <time dateTime='2021-01-27T16:35'>
-                    Wednesday at 4:35pm
-                  </time>
-                </p>
-              </div>
-              <div className='mt-4 space-y-6 text-sm text-gray-800'>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Malesuada at ultricies tincidunt elit et, enim. Habitant nunc, adipiscing non fermentum, sed est a, aliquet. Lorem in vel libero vel augue aliquet dui commodo.
-                </p>
-                <p>
-                  Nec malesuada sed sit ut aliquet. Cras ac pharetra, sapien purus vitae vestibulum auctor faucibus ullamcorper. Leo quam tincidunt porttitor neque, velit sed. Tortor mauris ornare ut tellus sed aliquet amet venenatis condimentum. Convallis accumsan et nunc eleifend.
-                </p>
-                <p>
-                  <strong className='font-semibold'>
-                    Monica White
-                  </strong>
-                  Customer Service
-                </p>
-              </div>
-            </li>
-
-            <li className='bg-white px-4 py-6 shadow sm:rounded-lg sm:px-6'>
-              <div className='sm:flex sm:justify-between sm:items-baseline'>
-                <h3 className='text-base font-medium'>
-                  <span className='text-gray-900'>
-                    Joe Armstrong
-                  </span>
-                  <span className='text-gray-600'>
-                    wrote
-                  </span>
-                </h3>
-                <p className='mt-1 text-sm text-gray-600 whitespace-nowrap sm:mt-0 sm:ml-3'>
-                  <time dateTime='2021-01-27T16:09'>
-                    Wednesday at 4:09pm
-                  </time>
-                </p>
-              </div>
-              <div className='mt-4 space-y-6 text-sm text-gray-800'>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Malesuada at ultricies tincidunt elit et, enim. Habitant nunc, adipiscing non fermentum, sed est a, aliquet. Lorem in vel libero vel augue aliquet dui commodo.
-                </p>
-                <p>
-                  Nec malesuada sed sit ut aliquet. Cras ac pharetra, sapien purus vitae vestibulum auctor faucibus ullamcorper. Leo quam tincidunt porttitor neque, velit sed. Tortor mauris ornare ut tellus sed aliquet amet venenatis condimentum. Convallis accumsan et nunc eleifend.
-                </p>
-                <p>
-                  – Joe
-                </p>
-              </div>
-            </li>
-          </ul>
         </div>
       </section>
 
