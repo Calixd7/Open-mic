ebaea75b8509c7084b44bbbf5858db52f8898167
Daniel McKinney <adrmckinney@gmail.com>
2021-03-04 16:43:08 -0500

    fixed it

--- frontend/src/components/HeaderMobile.js
+++ frontend/src/components/HeaderMobile.js
@@ -12,7 +12,7 @@ const HeaderMobile = () => {
         <Link
           to='/friends'
           className='text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-        >Friends
+        >Following
         </Link>
         <Link
           to='/message'
