2bf2ddbd8c4d63d72591537ec2bc03ee815651fe
Daniel McKinney <adrmckinney@gmail.com>
2021-03-07 10:27:17 -0500

    made changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -193,7 +193,7 @@ export function getConnections (token) {
 }
 
 export function searchProfiles (token, pendingSearch, vacancy, instrument, wantedInstrument) {
-  return apiUrl.get(`api/userprofiles/?search=${pendingSearch}&vacancy=${vacancy}&instrument=${instrument}&wanted_instrument=${wantedInstrument}`, {
+  return apiUrl.get(`api/userprofiles/?search=${pendingSearch}&vacancy=${vacancy}&instrument=${instrument}&Wanted_instrument=${wantedInstrument}`, {
     headers: {
       Authorization: `Token ${token}`
     }
