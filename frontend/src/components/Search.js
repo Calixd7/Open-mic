b33dfbfcefa1f23f37af365d34098e562afc09bf
Daniel McKinney <adrmckinney@gmail.com>
2021-03-08 01:57:45 -0500

    added some search features

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -150,9 +150,9 @@ const Search = ({ token, setCards, setShowSearch, status, setStatus, genre, setG
               onClick={() => setShowSearch(false)}
             >
               <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' className='ml-0.5 mr-2 h-4 w-4'>
-                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z' />
+                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15' />
               </svg>
-              <span>Close</span>
+              <span>Clear</span>
             </button>
           </div>
         </div>
