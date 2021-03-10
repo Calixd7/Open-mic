766bae2fd19b22ddcd2c08e5c1e3e669c4d2d9cd
Daniel McKinney <adrmckinney@gmail.com>
2021-03-09 19:45:40 -0500

    turned off disabled on profile submit

--- frontend/src/components/ProfileSetup.js
+++ frontend/src/components/ProfileSetup.js
@@ -111,9 +111,9 @@ const ProfileSetup = ({ token, profile, userType, isEditing, setIsImage, setAvat
   // console.log('token in ProfileSetup', token)
   // console.log('vacancy', vacancy)
   // console.log('safeProfile.pk', safeProfile.pk)
-
   console.log('instruments', instruments)
   console.log('genres', genres)
+  console.log('wantedInstruments', wantedInstruments)
   console.log('genreForApi(genres)', genreForApi(genres))
   console.log('spotify', spotify)
 
@@ -236,7 +236,7 @@ const ProfileSetup = ({ token, profile, userType, isEditing, setIsImage, setAvat
             <div className='mt-4'>
               <button
                 className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                disabled={disableSubmit}
+                // disabled={disableSubmit}
                 type='submit'
               >
                 {isEditing
