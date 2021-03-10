a48a086f3491e3cd576dcc8bd670cbd8088144fb
Daniel McKinney <adrmckinney@gmail.com>
2021-03-10 01:56:42 -0500

    fixed bug that wouldn't let people setup their profile if they skipped it on initial login.

--- frontend/src/components/ProfileSetup.js
+++ frontend/src/components/ProfileSetup.js
@@ -168,7 +168,11 @@ const ProfileSetup = ({ token, profile, userType, isEditing, setIsImage, setAvat
     <div className='min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8'>
       <div className='max-w-md w-full space-y-8'>
         <div>
-          <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>Profile Setup</h2>
+          <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
+            {isEditing
+              ? 'Update Profile'
+              : 'Profile Setup'}
+          </h2>
         </div>
         <div className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
           <form
@@ -182,7 +186,7 @@ const ProfileSetup = ({ token, profile, userType, isEditing, setIsImage, setAvat
             <div className='flex flex-col'>
 
               <div className='mt-4'>
-                <Status status={status} setStatus={setStatus} />
+                <Status status={status} setStatus={setStatus} isEditing={isEditing} />
               </div>
 
               <div className='mt-4'>
