5624d16cb3aeaa17c45d556a0e99bbdeecd8d0d6
Daniel McKinney <adrmckinney@gmail.com>
2021-03-10 19:45:45 -0500

    conflict

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -139,14 +139,10 @@ function Header ({ username, token, setToken, isLoggedIn, pk, isImage, setIsImag
               {showSearch
                 ? <svg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='currentColor' className='ml-0.5 mr-4 h-8 w-8'>
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z' />
-<<<<<<< HEAD
-                  </svg>
-=======
                 </svg>
->>>>>>> 9f08f56bc4915fee2c9f6a4abca8444f12981977
                 : <svg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='currentColor' className='ml-0.5 mr-4 h-6 w-6'>
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z' />
-                </svg>}
+                  </svg>}
 
             </button>
             <div className='hidden sm:block'>
